0356fab84bfab183fa47b9177abdc160deb9ce46
silahouk <silahouk@gmail.com>
2019-12-16 18:24:28 +0700

    New folder

---
+++ view/club.php
@@ -0,0 +1,43 @@
+<form action="">
+    <div class="card">
+        <div class="card-header">Register</div>
+        <div class="card-body">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="user" id="user" value="Username">
+            </div>
+            <div class="form-group">
+                <label>Gender</label>
+                <input type="radio" name="gender" id="gender" value="Male">Male
+                <input type="radio" name="gender" id="gender" value="Female">Female
+            </div>
+            <div class="form-group">
+                <label>Age</label>
+                <input type="number" name="age" id="age" value="Age">
+            </div>
+            <div class="form-group">
+                <label>Height</label>
+                <input type="text" name="height" id="height" value="Height">
+            </div>
+            <div class="form-group">
+                <label>Role</label>
+                <select name="role" id="role">
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="email" name="email" id="email" value="Email">
+            </div>
+            <div class="form-group">
+                <label>Address</label>
+                <input type="text" name="address" id="address" value="Address">
+            </div>
+            <div class="form-group">
+                <label>Description</label>
+                <input type="text" name="description" id="description" value="Description">
+            </div>
+        </div>
+    </div>
+</form>
---
+++ view/template.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="view/bootstrap/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+    <script src="view/bootstrap/js/bootstrap.min.js"></script>
+    <title>Project PHP</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <img src="" alt="">
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="club.php">Club</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
